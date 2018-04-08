fcfe3a312feda54ca5dfa1be8bd6ae9f8fa6d063
Marcin Kornek <marcin.kornek@netguru.pl>
2018-04-08 17:46:47 +0200

    Add a first mobx app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,65 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import { observer } from 'mobx-react';
+import { decorate, observable, computed, action } from "mobx"
+import DevTools from 'mobx-react-devtools';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+class TodoList {
+  todos = [
+    { id: 1, title: 'todo1', finished: false },
+    { id: 1, title: 'todo1', finished: true }
+  ];
+
+  get unfinishedTodoCount() {
+    return this.todos.filter(todo => !todo.finished).length;
+  }
+
+  get finishedTodoCount() {
+    return this.todos.filter(todo => todo.finished).length;
   }
 }
 
-export default App;
+decorate(TodoList, {
+  todos: observable,
+  unfinishedTodoCount: computed,
+  finishedTodoCount: computed,
+})
+
+const TodoListView = observer(class TodoListView extends Component {
+  render() {
+    const { todoList } = this.props
+
+    return (
+      <div>
+        <ul>
+          {todoList.todos.map(todo =>
+            <TodoView todo={todo} key={todo.id} />
+          )}
+        </ul>
+        <div>Tasks left: {todoList.unfinishedTodoCount}</div>
+        <div>Tasks finished: {todoList.finishedTodoCount}</div>
+      </div>
+    )
+  }
+})
+
+const TodoView = observer(({todo}) =>
+  <li>
+    <input
+      type="checkbox"
+      checked={todo.finished}
+      onClick={() => todo.finished = !todo.finished}
+    />
+    {todo.title}
+  </li>
+)
+
+const todoList = new TodoList();
+
+const App = () => (
+  <Fragment>
+    <TodoListView todoList={todoList} />
+    <DevTools />
+  </Fragment>
+)
+
+export default App
